94094cdf0fa9d4ebb3bab013c073beb602299886
MarieSerroyen <marie.serroyen@gmail.com>
2022-11-28 15:39:57 +0100

    feat: made create donut controller

---
+++ controllers/donuts.js
@@ -0,0 +1,30 @@
+const Donut = require('../models/donut');
+
+//create donut
+function create(req, res) {
+    let name = req.body.name;
+
+    let donut = new Donut();
+    donut.name = name;
+
+    donut.save((err, donut) => {
+        if(err) {
+            let result = {
+                status: 'error',
+                message: err.message
+            }
+            res.json(result);
+        } else {
+            let result = {
+                status: 'success',
+                message: 'Donut created',
+                data: {
+                    donut: donut
+                }
+            }
+            res.json(result);
+        }
+    }); 
+}
+
+module.exports.create = create;
