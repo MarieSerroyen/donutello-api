8d6196ecd39ad69450e0f29df7a53fec9a347886
MarieSerroyen <marie.serroyen@gmail.com>
2022-11-28 15:41:01 +0100

    feat: add create controller to router

--- routes/donuts.js
+++ routes/donuts.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const donutController = require('../controllers/donuts');
 
+router.post('/', donutController.create);
+
 module.exports = router;
