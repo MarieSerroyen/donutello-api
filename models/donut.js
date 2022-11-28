3c19c75941cb0cecccb05df1a87fcbe9ce362223
MarieSerroyen <marie.serroyen@gmail.com>
2022-11-28 16:36:45 +0100

    feat: expand donutSchema with extra donut information

--- models/donut.js
+++ models/donut.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const donutSchema = new Schema({
-    name: {type: String, required: true},
+    name: String,
+    base: String,
+    glaze: String,
+    logo: String,
+    sprinkles: Boolean,
+    amount: Number,
+    date: Date,
+    description: String,
+    status: String
 });
 
 const Donut = mongoose.model('Donut', donutSchema);
