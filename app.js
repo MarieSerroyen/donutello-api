6f663249a211592e950fc958ee0bc1aaa4729dd6
MarieSerroyen <marie.serroyen@gmail.com>
2022-11-28 15:35:25 +0100

    feat: add api route to app.js

--- app.js
+++ app.js
@@ -7,11 +7,10 @@ var logger = require('morgan');
 const cors = require('cors');
 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const donuts = require('./routes/donuts');
 
 mongoose.connect('mongodb://localhost:27017/donuttello')
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,8 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/donuts', donuts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/donuts.js
+++ routes/donuts.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const router = express.Router();
+const donutController = require('../controllers/donuts');
 
 module.exports = router;
