c63512088c4e4d4832c0160dbfcc1ea804e35035
MarieSerroyen <marie.serroyen@gmail.com>
2022-11-28 15:24:10 +0100

    feat: add local database connection with mongoose

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var createError = require('http-errors');
 var express = require('express');
+const mongoose = require('mongoose');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+mongoose.connect('mongodb://localhost:27017/donuttello')
 var app = express();
 
 // view engine setup
