fa8c4788565e1bf6af1a76cfc2cc078f7fbf1fc3
MarieSerroyen <marie.serroyen@gmail.com>
2022-11-29 17:10:30 +0100

    feat: add passport authenticate to status update route

--- routes/donuts.js
+++ routes/donuts.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('./../passport/passport');
 const donutController = require('../controllers/donuts');
 
 router.post('/', donutController.create);
-router.put('/:id', donutController.updateDonutStatus);
+router.put('/:id', passport.authenticate('jwt', {session: false}), donutController.updateDonutStatus);
 
 module.exports = router;
