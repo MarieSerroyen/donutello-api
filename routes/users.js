5ae0239e35c1d4b1261106ac063dae58af4b68f5
LarissaDeBorgher <r0839981@student.thomasmore.be>
2022-11-30 11:52:44 +0100

    Feat: add router change password

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/users');
 
 router.post('/', userController.create);
 router.post('/login', userController.login);
+router.put('/:id', userController.changePassword);
 
 module.exports = router;
