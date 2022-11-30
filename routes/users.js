ae7f8f3d7c3407eb205c844b3be8d859b29814bf
LarissaDeBorgher <r0839981@student.thomasmore.be>
2022-11-30 11:54:38 +0100

    Feat: add authentication to change password router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/users');
+const passport = require('./../passport/passport');
 
 router.post('/', userController.create);
 router.post('/login', userController.login);
-router.put('/:id', userController.changePassword);
+router.put('/:id',passport.authenticate('jwt', {session: false}), userController.changePassword);
 
 module.exports = router;
