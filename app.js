10349e874880125bf235f5504c9b5e837c129093
MarieSerroyen <marie.serroyen@gmail.com>
2022-12-19 08:12:03 +0100

    feat: try different cors code

--- app.js
+++ app.js
@@ -18,9 +18,22 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+const corsOpts = {
+  origin: '*',
+
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+
 app.use(logger('dev'));
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOpts));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
