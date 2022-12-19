e4038f6745d89c9f5ad448228b14c34f3398a5a9
MarieSerroyen <marie.serroyen@gmail.com>
2022-12-19 08:23:21 +0100

    feat: try different cors code to fix cors error

--- app.js
+++ app.js
@@ -41,6 +41,13 @@ app.use(
 app.use('/api/v1/donuts', donutRouter);
 app.use('/api/v1/users', userRouter);
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
